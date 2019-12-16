b90be9d4f0283c429648e330c785c973b69b7ea6
Vrobin0101 <robin.vrtz@gmail.com>
2019-12-16 18:16:41 +0100

    all

--- sources/sort_way.c
+++ sources/sort_way.c
@@ -6,13 +6,13 @@
 /*   By: vrobin <vrobin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 15:20:38 by vrobin            #+#    #+#             */
-/*   Updated: 2019/12/16 17:13:44 by vrobin           ###   ########.fr       */
+/*   Updated: 2019/12/16 18:16:30 by vrobin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "V3_lem_in.h"
 
-uint_fast8_t	is_collision(t_path way1, t_path way2)
+uint_fast8_t	is_collision(t_lem_in *s, size_t *queue, size_t current)
 {
 	ssize_t i;
 	ssize_t j;
@@ -52,45 +52,47 @@ void		sort_way(t_lem_in *s)
 	}
 }
 
+/*
+
+recup link to start et end
+recup le best_case actuel (le plus court (chemin[0]) + nb_fourmis)
+tant que le prochain meilleur cas possible peut etre mieux que lancien
+tenter de mettre nv chemin tant que on a pas atteint max_path:
+> collision = next chemin
+> pas collision = assigner 
+calculer le coups
+si meilleur -> changer, si pire continuer
+
+*/
+
 ssize_t		*get_way(t_lem_in *s)
 {
 	size_t	i; // current path
 	size_t	j; // other path to add
 	size_t	max_path; // room link to start && end
-	ssize_t	*ret; // return tab (with indexs of all path who will be used)
+	ssize_t	*current; // return tab (with indexs of all path who will be used)
 	size_t	best_case; // best case found
-	size_t	best_path; // best path int the current case
-	size_t	act_ant; // ant left to use
+	// size_t	act_ant; // ant left to use
 	size_t	act_path; // index of current number of path
-	size_t	new_case; // new current case 
+	// size_t	act_case; // current case 
 
 	i = 0;
-	new_case = 0;
 	max_path = s->room_tab[s->start].link_rm;
+	current = NULL;
 	if (max_path > s->room_tab[s->end].link_rm)
 		max_path = s->room_tab[s->start].link_rm;
-	if (!(ret = malloc(sizeof(ssize_t) * max_path + 1)))
-		exit_failure(s, 123, "can't malloc ret", 0);
+	if (!(current = malloc(sizeof(ssize_t) * max_path + 1)))
+		exit_failure(s, 123, "can't malloc current", 0);
+	ft_printf("max_path %d\n", max_path);
 	while (i <= max_path)
-		ret[i++] = -1;
+		current[i++] = -1;
 	i = 0;
 	best_case = s->way[0].last_node + s->ant - 1;
-	while (i <= s->p_last && s->way[i].last_node * max_path < best_case)
+	max_path -= 1;
+	while (i < s->p_last)
 	{
-		j = i + 1;
-		act_path = 0;
-		act_ant = s->ant;
-		best_path = s->way[i].last_node;
-		while (act_path <= max_path && j <= s->p_last)
-		{
-			if (is_collision(s->way[i], s->way[j]) == 0)
-			{
-				act_path++;
-			}
-			j++;
-		}
-		i++;
+		if (is_collision(s, current, j) == 0) 
+			act_path++;
 	}
-	ft_printf("%d\n", best_case);
-	return (ret);
+	return (current);
 }
